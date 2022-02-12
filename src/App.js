ebec0af274145bedb3696ff84d2544d2a6cfcb3b
Rodolfo Gaspar Paredes <gasparianprds@gmail.com>
2022-02-11 22:32:08 -0300

    Install de Bootstrap-React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
+
 
 function App() {
   return (
