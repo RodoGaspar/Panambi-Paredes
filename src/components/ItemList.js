27c7fee339f565cb76c0be0b9947611300baf4cb
Rodolfo Gaspar Paredes <gasparianprds@gmail.com>
2022-03-20 23:45:59 -0300

    BSGrid en itemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,24 @@
 import {Item} from "./Item";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const ItemList = ({prods}) => {
     return(
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alingnItems: 'center',
-            }}
-        >
-            {prods.map((producto) => (
-                    <Item {...producto} key={producto.id}/>    
-            ))}
-        </div>
+        <Container>
+            <Row
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alingnItems: 'center',
+                }}
+            >
+                {prods.map((producto) => (
+                    <Col s={12} md={3} lg={3}>
+                        <Item {...producto} key={producto.id}/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 };
--- src/components/Navibar/NaviBar.js
+++ src/components/Navibar/NaviBar.js
@@ -9,11 +9,11 @@ import { NavLink } from "react-router-dom";
 
 export const NaviBar = () => {
     return (    
-        <Navbar collapseOnSelect expand="lg"  className='bg-cabezon'>
+        <Navbar collapseOnSelect expand="md"  className='bg-cabezon'>
             <Container>
                 <Navbar.Brand href="/"><img className="logopanambi" src={LogoPanambi} alt="Logo de Panambí"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" variant="success">
+                <Navbar.Collapse  bg="success" id="basic-navbar-nav" variant="success">
                 <Nav className="navegador">
                     <NavLink className="navLink" to={"/"}>Inicio</NavLink>
                     <NavLink className="navLink" to={"/category/maceta"}>Macetas</NavLink>
